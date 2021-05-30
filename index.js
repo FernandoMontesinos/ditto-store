03ca33ec7de25d6d8055dbad574ea10649e2e7c4
Fernancode548 <fmontesinosestrada@gmail.com>
2021-05-29 23:04:33 -0500

    segunda version

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const productoscaja = document.getElementById('contenedor-productos');
 const productos = document.getElementById('productos');
 const footer = document.getElementById('footer');
+const navC = document.getElementById('carritonav');
 const templateProducto = document.getElementById('producto-template').content; // Aca poner Content xd
 const templateFooter = document.getElementById('footer-template').content;
 const templateCarrito = document.getElementById('carrito-template').content;
+const templateNavegacionCantidad = document.getElementById('template-navcantidad').content;
 //const cantidadNavegacion = document.getElementById('cantidadnav').content;
 
 const fragment = document.createDocumentFragment();
@@ -13,7 +15,7 @@ let carrito = {}
 document.addEventListener('DOMContentLoaded', () => {
     fetchData();
     if(localStorage.getItem('carrito')) {
-        carrito = JSON.parse(localStorage.getItem('carrito'))
+        carrito = JSON.parse(localStorage.getItem('carrito'));
         inyectarCarrito()
     }
 })
@@ -65,8 +67,9 @@ const modificarCarrito = objeto => {
         id: objeto.querySelector('.formulario__submit').dataset.id,
         nombre: objeto.querySelector('#nombre').textContent,
         precio: objeto.querySelector('#precio').textContent,
-        cantidad: 1
-        // imagen: objeto.getElementById('imagen').setAttribute("src", producto.imagen), // Para luego poner una imagen minimizada   
+        cantidad: 1,
+        // imagen: objeto.getElementById('imagen').setAttribute("src")  
+        // imagen: objeto.getElementById('imagen').setAttribute("src", producto.imagen), // Para luego poner una imagen minimizada 
     }
     // Verificar si ya existe uno anteriormente para agregarle otro
     if (carrito.hasOwnProperty(producto.id)) {
@@ -90,6 +93,8 @@ const inyectarCarrito = () => {
         templateCarrito.getElementById('carrito-cantidad').textContent = producto.cantidad
         templateCarrito.querySelector('.agregar-carrito').dataset.id = producto.id
         templateCarrito.querySelector('.restar-carrito').dataset.id = producto.id
+
+        //templateCarrito.querySelector('.imagen__carrito').setAttribute("src", producto.imagen)
         const resultado = parseInt(producto.precio * producto.cantidad);
         templateCarrito.querySelector('span').textContent = resultado // * producto.cantidad
    
@@ -105,10 +110,12 @@ const inyectarCarrito = () => {
 
 const pintarFooter = () => {
     footer.innerHTML = '';
+    navC.innerHTML = '';
     if(Object.keys(carrito).length === 0){
         footer.innerHTML = `
         <th scope="row" colspan="5" class="sin-productos">No hay productos Seleccionados</th>
-        `
+        `;
+        navC.innerHTML = `<span class="cero-nav">0</span>`
 
         return;
     }
@@ -118,13 +125,18 @@ const pintarFooter = () => {
 
     //console.log(nPrecio);
 
-    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
-    templateFooter.querySelector('span').textContent = nPrecio
+    templateNavegacionCantidad.querySelector('#cantidadM').textContent = nCantidad;
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad;
+    templateFooter.querySelector('span').textContent = nPrecio;
     //cantidadNavegacion.querySelector('.cantidadnav').textContent = nCantidad
 
     const clone = templateFooter.cloneNode(true);
     fragment.appendChild(clone)
-    footer.append(fragment) 
+    footer.append(fragment);
+    
+    const clone2 = templateNavegacionCantidad.cloneNode(true);
+    fragment.appendChild(clone2);
+    navC.append(fragment);
 
     const btnVaciar = document.getElementById('vaciar-carrito');
     btnVaciar.addEventListener('click', () => {
